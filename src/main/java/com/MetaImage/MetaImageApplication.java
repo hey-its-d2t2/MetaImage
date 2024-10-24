030e6c53739e8568eb65ac6c4ab630fd8133dfb2
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-24 10:25:45 +0530

    Initial commit: Project created from start.spring.io whit ver spring 3.3.4, Java 17, Maven, Jar with Dependency Starter Web, Lombok, Devtools, Thymeleaf.

---
+++ src/main/java/com/MetaImage/MetaImageApplication.java
@@ -0,0 +1,13 @@
+package com.MetaImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MetaImage/MetaImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MetaImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
