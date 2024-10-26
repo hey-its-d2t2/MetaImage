3316f614a1e81b4e672095ab3e43ca1f49fec3dc
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-26 16:42:46 +0530

    Feat: New response class added.

---
+++ src/main/java/com/MetaImage/dto/ExifMetadataResponse.java
@@ -0,0 +1,29 @@
+package com.MetaImage.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Getter
+@Data
+public class ExifMetadataResponse {
+    private Map<String, String> metadata;
+    public ExifMetadataResponse() {
+        this.metadata = new HashMap<>();
+    }
+
+    public void addMetadata(String key, String value) {
+        this.metadata.put(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "ExifMetadataResponse{" +
+                "metadata=" + metadata +
+                '}';
+    }
+}
